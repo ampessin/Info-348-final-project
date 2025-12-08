722a91c0a8d6740cf4649ef25b4ed1ec1cc2aedb
Praptipatel101 <praptipatel@umass.edu>
2025-12-07 20:15:33 -0500

    create file

---
+++ 348 final.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+
+BASE_URL = 'https://api.genius.com'
+TOKEN = 'vDa1JrM0TKY9CzIADy1rC94Y5I72-wZJOxiV9i3SVnzslJa0MSY5AWbl-qOczNOS'  # Fix this!
+HEADERS = {'Authorization': 'Bearer ' + TOKEN}  
